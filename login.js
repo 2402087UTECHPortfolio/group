14192c5c8e7228f839190ecf852290e67831d8fa
daynea-chambers <dayneachambers@students.utech.edu.jm>
2025-11-28 18:49:33 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,31 @@
 // === LOGIN PAGE VALIDATION ===
 document.getElementById("loginForm").addEventListener("submit", function(event){
-    event.preventDefault(); e
-
+    event.preventDefault(); 
+ 
     let trn= document.getElementById("trn").value;
     let password = document.getElementById("password").value.trim();
+    let attempts=0;
 
     if(trn=== "" || password === ""){
         alert("Please enter both trn and password.");
     } else {
-        alert("Login successful! (Demo only)");
+        let RegistrationData=JSON.parse(localStorage.getItem("RegistrationData");
+        if (RegistrationData.some(u=>u.trn==trn && u=>u.password===password)
+        {
+             alert("Login successful! (Demo only)");
+             window.location.href = "index.html";
+        }
+        else
+        {
+            alert("Incorrect username or password...try again");
+            attempts++;
+            if (attempts==3)
+            {
+               alert("Your account has been locked");
+            }
+        }
         
-        window.location.href = "index.html";
     }
 });
+
 
